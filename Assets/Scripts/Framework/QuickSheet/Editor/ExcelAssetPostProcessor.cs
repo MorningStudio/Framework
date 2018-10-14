3619beeb51f8c0dd8e971e20438c37875b9f135d
klchan <359121504@QQ.COM>
2018-10-15 00:12:50 +0800

    update

---
+++ Assets/Scripts/Framework/QuickSheet/Editor/ExcelAssetPostProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using UnityEditor;
+
+namespace MorningStudio
+{
+    public class ExcelAssetPostProcessor : AssetPostprocessor
+    {
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            foreach (string imported in importedAssets)
+            {
+                string fileExt = Path.GetExtension(imported);
+                if (fileExt.Equals(".xls") || fileExt.Equals(".xlsx"))
+                {
+                    ExcelImportWindow.Init();
+                }
+            }
+        }
+
+        private static void ProcessExcel()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Framework/QuickSheet/Editor/ExcelImportWindow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ExcelImportWindow : EditorWindow
+{
+    [MenuItem("Window/QuickSheet/Test")]
+    public static void Init()
+    {
+        ExcelImportWindow window = EditorWindow.GetWindow<ExcelImportWindow>();
+        window.titleContent = new GUIContent("Excel Import Setting");
+        window.Show();
+        
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+}
