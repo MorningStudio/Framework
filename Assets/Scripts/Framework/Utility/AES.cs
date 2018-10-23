d813e90e177da25a3592c3bca1f6bc0711be8d05
AngryRookie <359121504@QQ.COM>
2018-10-23 20:07:12 +0800

    add AES

---
+++ Assets/Scripts/Framework/Utility/AES.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MorningStudio
+{
+    public static partial class Utility
+    {
+        public static class AES
+        {
+            /// <summary>
+            /// 默认盐
+            /// </summary>
+            public const string DefaultSalt = "jmU2DQotw6GegAGKCqSdHg==";
+
+            /// <summary>
+            /// 根据密码创建指定长度的密钥
+            /// </summary>
+            /// <param name="password">密码</param>
+            /// <param name="keySize">长度</param>
+            /// <param name="salt">盐</param>
+            /// <returns></returns>
+            public static byte[] CreateKey(string password,int keySize,string salt = "")
+            {
+                if(string.IsNullOrEmpty(salt))
+                {
+                    salt = DefaultSalt;
+                }
+                DeriveBytes derivedKey = new Rfc2898DeriveBytes(password, Convert.FromBase64String(salt));
+                return derivedKey.GetBytes(keySize);
+            }
+
+            /// <summary>
+            /// 加密
+            /// </summary>
+            /// <param name="inputData">明文</param>
+            /// <param name="key">密钥</param>
+            /// <param name="iV">向量</param>
+            /// <returns>密文</returns>
+            public static byte[] EncryptStringToBytes(string inputData, byte[] key, byte[] iV)
+            {
+                if (inputData == null || inputData.Length <= 0)
+                    throw new ArgumentNullException("inputData");
+                if (key == null || key.Length <= 0)
+                    throw new ArgumentNullException("key");
+                if (iV == null || iV.Length <= 0)
+                    throw new ArgumentNullException("iV");
+                byte[] encrypted;
+                using (RijndaelManaged rijAlg = new RijndaelManaged())
+                {
+                    rijAlg.Key = key;
+                    rijAlg.IV = iV;
+                    rijAlg.Mode = CipherMode.CBC;
+                    rijAlg.Padding = PaddingMode.PKCS7;
+                    ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+                    using (MemoryStream msEncrypt = new MemoryStream())
+                    {
+                        using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                            {
+                                swEncrypt.Write(inputData);
+                            }
+                            encrypted = msEncrypt.ToArray();
+                        }
+                    }
+                }
+                return encrypted;
+            }
+
+            /// <summary>
+            /// 解密
+            /// </summary>
+            /// <param name="inputData">密文</param>
+            /// <param name="key">密钥</param>
+            /// <param name="iV">向量</param>
+            /// <returns>明文</returns>
+            public static string DecryptStringFromBytes(byte[] inputData, byte[] key, byte[] iV)
+            {
+                if (inputData == null || inputData.Length <= 0)
+                    throw new ArgumentNullException("inputData");
+                if (key == null || key.Length <= 0)
+                    throw new ArgumentNullException("key");
+                if (iV == null || iV.Length <= 0)
+                    throw new ArgumentNullException("iV");
+                string plaintext = null;
+                using (RijndaelManaged rijAlg = new RijndaelManaged())
+                {
+                    rijAlg.Key = key;
+                    rijAlg.IV = iV;
+                    ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+                    using (MemoryStream msDecrypt = new MemoryStream(inputData))
+                    {
+                        using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                            {
+                                plaintext = srDecrypt.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+                return plaintext;
+            }
+        }
+    }
+}
--- Assets/Scripts/Test/Test.cs
+++ Assets/Scripts/Test/Test.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using MorningStudio;
 using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System;
 
 public class DataModel
 {
@@ -17,34 +20,44 @@ public class Test : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        DataModel dataModel = new DataModel();
-        dataModel.id = 1;
-        dataModel.name = "klchan";
-        SettingManager.Instance.SetObject("obj",dataModel);
-        SettingManager.Instance.Save();
-        DataModel obj = SettingManager.Instance.GetObject<DataModel>("obj");
-        Log.Info(obj.name);
+        //DataModel dataModel = new DataModel();
+        //dataModel.id = 1;
+        //dataModel.name = "klchan";
+        //SettingManager.Instance.SetObject("obj",dataModel);
+        //SettingManager.Instance.Save();
+        //DataModel obj = SettingManager.Instance.GetObject<DataModel>("obj");
+        //Log.Info(obj.name);
 
-        EventManager.Instance.AddListener(TestEventArgs.EventId,OnTestHandle);
-        TestEventArgs args = new TestEventArgs();
-        args.message = "Hello";
-        EventManager.Instance.DispatchNow(this, args);
-        EventManager.Instance.RemoveListener(TestEventArgs.EventId, OnTestHandle);
-        EventManager.Instance.DispatchNow(this, args);
+        //EventManager.Instance.AddListener(TestEventArgs.EventId,OnTestHandle);
+        //TestEventArgs args = new TestEventArgs();
+        //args.message = "Hello";
+        //EventManager.Instance.DispatchNow(this, args);
+        //EventManager.Instance.RemoveListener(TestEventArgs.EventId, OnTestHandle);
+        //EventManager.Instance.DispatchNow(this, args);
 
-        MethodInfo methodInfo = typeof(Test).GetMethod("Test1");
-        try
-        {
-            methodInfo.Invoke(null, new object[] { 1, 2 });
-        }
-        catch (System.ArgumentException)
-        {
-            Debug.LogWarning("Args not match");
-        }
+        //MethodInfo methodInfo = typeof(Test).GetMethod("Test1");
+        //try
+        //{
+        //    methodInfo.Invoke(null, new object[] { 1, 2 });
+        //}
+        //catch (System.ArgumentException)
+        //{
+        //    Debug.LogWarning("Args not match");
+        //}
 
+        byte[] key = Utility.AES.CreateKey("klchan",32);
+        byte[] iV = Utility.AES.CreateKey("25ss",16);
+
+        string text = "1234567890testsets:~!@#$%^&*()_+";
+
+        byte[] encrypted = Utility.AES.EncryptStringToBytes(text, key, iV);
+        string decrypted = Utility.AES.DecryptStringFromBytes(encrypted, key, iV);
+        Debug.LogWarning(string.Format("Original:{0}", text));
+        Debug.LogWarning(string.Format("Encrypted:{0}", Convert.ToBase64String(encrypted)));
+        Debug.LogWarning(string.Format("decrypted:{0}", decrypted));
     }
-	
-	private void OnTestHandle(object sender,GameEventArgs e)
+
+    private void OnTestHandle(object sender,GameEventArgs e)
     {
         TestEventArgs args = e as TestEventArgs;
         Log.Info(args.message);
